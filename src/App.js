dd5e3084d9e5ee7ba1038eede5bb08e46ae90066
Jnmendza <jonatmendoza@gmail.com>
2019-10-09 15:27:00 -0700

    setup, folders, index are up

--- src/App.js
+++ src/App.js
@@ -4,42 +4,36 @@ import Header from './components/Header';
 import AddedFeatures from './components/AddedFeatures';
 import AdditionalFeatures from './components/AdditionalFeatures';
 import Total from './components/Total';
+import { dispatch } from 'rxjs/internal/observable/range';
+
+import { featureReducer } from './reducers'
+import { createStore } from 'redux';
+
+
+const store = createStore(featureReducer);
 
 const App = () => {
-  const state = {
-    additionalPrice: 0,
-    car: {
-      price: 26395,
-      name: '2019 Ford Mustang',
-      image:
-        'https://cdn.motor1.com/images/mgl/0AN2V/s1/2019-ford-mustang-bullitt.jpg',
-      features: []
-    },
-    additionalFeatures: [
-      { id: 1, name: 'V-6 engine', price: 1500 },
-      { id: 2, name: 'Racing detail package', price: 1500 },
-      { id: 3, name: 'Premium sound system', price: 500 },
-      { id: 4, name: 'Rear spoiler', price: 250 }
-    ]
-  };
+  
 
   const removeFeature = item => {
     // dispatch an action here to remove an item
+
   };
 
   const buyItem = item => {
-    // dipsatch an action here to add an item
+    dispatch({type: '', payload: item})
+    // dispatch an action here to add an item
   };
 
   return (
     <div className="boxes">
       <div className="box">
-        <Header car={state.car} />
-        <AddedFeatures car={state.car} />
+        <Header car={store.car} />
+        <AddedFeatures car={store.car} />
       </div>
       <div className="box">
-        <AdditionalFeatures store={state.store} />
-        <Total car={state.car} additionalPrice={state.additionalPrice} />
+        <AdditionalFeatures store={store} />
+        <Total car={store.car} additionalPrice={store.additionalPrice} />
       </div>
     </div>
   );
